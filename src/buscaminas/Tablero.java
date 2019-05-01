59cb09332b9f5cfc8283b8af8a38ff2f3b235ee5
adrishg <adri315289837@gmail.com>
2019-05-01 17:11:08 -0500

    Tablero abstacción rara

--- src/buscaminas/Tablero.java
+++ src/buscaminas/Tablero.java
@@ -12,7 +12,10 @@ import java.util.ArrayList;
  * @author LENOVO
  */
 public class Tablero {
-    public ArrayList<ArrayList<Celda>> tablero_juego;
+    public int tamanio;
+    //public int filas;
+    public int numMinas;
+    //public ArrayList<ArrayList<Celda>> tablero_juego;
     
 }
 //Constructor
@@ -20,6 +23,21 @@ public class Tablero {
     
 //}
 
-//Getters 
- public ArrayList Inicia
 
+//Getters 
+
+public Tablero(int tamanio, int numMinas){
+    this.tamanio = tamanio;
+    this.numMinas = numMinas;
+}
+
+public ArrayList<ArrayList<Celda>> creadorTablero(Tablero){
+    ArrayList<ArrayList<Celda>> tablero_juego = new ArrayList<ArrayList<Celda>>;
+    for(int i=0; i<9; i++){
+        for(int j=0; j<9; j++){
+            tablero_juego[][].add( new Celda(false,0));
+    }
+return tablero_juego;
+}
+}
+    
